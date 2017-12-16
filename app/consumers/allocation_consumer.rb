5cc88871f87d3895950e865f90a64a2de843a079
umarkotak <umarkotak@yahoo.co.id>
2017-12-16 17:18:31 +0700

    microservices complete

--- app/consumers/allocation_consumer.rb
+++ app/consumers/allocation_consumer.rb
@@ -6,15 +6,17 @@ class AllocationConsumer < Racecar::Consumer
 
     if data[:action] == 'set_driver_location'
       set_driver_location(data)
-      puts "========================="
-      puts "DATA = #{data}"
-      puts "========================="
     elsif data[:action] == 'unset_driver_location'
       unset_driver_location(data)
-      puts "========================="
-      puts "DATA = #{data}"
-      puts "========================="
+    elsif data[:action] == 'get_driver'
+      get_driver(data)
+    elsif data[:action] == 'set_driver_location_done'
+      set_driver_location_done(data)
     end
+
+    puts "========================="
+    puts "DATA = #{data}"
+    puts "========================="
   end
 
   def set_driver_location(data)
@@ -22,6 +24,7 @@ class AllocationConsumer < Racecar::Consumer
     @driver_location.destroy if @driver_location
 
     @driver_location = DriverLocation.new(data[:driver_location])
+    # @driver_location.status = 'online'
     @driver_location.save
   end
 
@@ -29,5 +32,92 @@ class AllocationConsumer < Racecar::Consumer
     @driver_location = DriverLocation.find_by(driver_id: data[:driver_id])
     @driver_location.status = 'offline'
     @driver_location.save
+  end
+
+  def get_driver(data)
+    driver_locations = DriverLocation.where("status = 'online'")
+    driver_locations = driver_locations.where("service_type = '#{data[:service_type]}'")
+
+    driver_locations = driver_locations.select do |driver_location|
+      destination = [driver_location.lat.to_f, driver_location.lng.to_f]
+      origin = [data[:origin][:lat], data[:origin][:lng]]
+      get_distance(origin, destination) < 20.0
+    end
+
+    driver_location = driver_locations.sample
+
+    if driver_location
+      driver_found(driver_location, data)
+    else
+      driver_not_found(data)
+    end
+  end
+
+  def driver_found(driver_location, data)
+    driver_location.order_id = data[:order_id]
+    driver_location.status = 'busy'
+    driver_location.save
+
+    # order = Order.find(data[:order_id])
+    # order.driver_id = driver_location.driver_id
+    # order.save
+
+    send_driver_id_to_application(data[:order_id], driver_location.driver_id)
+
+    puts "=========================================="
+    puts "CONVERTED DATA      = #{data}"
+    puts "DRIVER ID           = #{driver_location.driver_id}"
+    puts "=========================================="
+  end
+
+  def driver_not_found(data)
+    sleep(3)
+    kafka = Kafka.new(
+      seed_brokers: ['127.0.0.1:9092'],
+      client_id: 'goride',
+    )
+    kafka.deliver_message("#{data}", topic: 'driver_location')
+
+    puts "=========================================="
+    puts "UNAVAILABLE DRIVER AT THE MOMENT"
+    puts "=========================================="
+  end
+
+  def send_driver_id_to_application(order_id, driver_id)
+    kafka = Kafka.new(
+      seed_brokers: ['127.0.0.1:9092'],
+      client_id: 'goride',
+    )
+    data = {}
+    data[:action] = 'send_driver_id'
+    data[:order_id] = order_id
+    data[:driver_id] = driver_id
+      
+    kafka.deliver_message("#{data}", topic: 'application')
+  end
+
+  def set_driver_location_done(data)
+    driver_location = DriverLocation.find_by(driver_id: data[:driver_id])
+    driver_location.order_id = nil
+    driver_location.status = 'online'
+    driver_location.save
+  end
+
+  def get_distance(loc1, loc2)
+    rad_per_deg = Math::PI/180  # PI / 180
+    rkm = 6371                  # Earth radius in kilometers
+    rm = rkm * 1000             # Radius in meters
+
+    dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
+    dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
+
+    lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
+    lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+    result = rm * c # Delta in meters
+    result = result / 1000
   end
 end
